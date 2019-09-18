29385903dca35c5353dd82bdaabf1b3154cb00b0
chengkrilo <krilo_c@hotmail>
2019-09-18 21:51:46 +0800

    init

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import {Switch , Route, Link} from 'react-router-dom';
+
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+
+// const HomePage = (props) =>{
+//     return(<div>
+//         <Link to='/topics'>Topics</Link>
+//         <button onClick={ ()=>{props.history.push('/topics')}}>history</button>
+//         <h1>HOME PAGE</h1>
+//     </div>);
+// };
+
+const TopicsList = () =>{
+    return(<div>
+        <h1>TopicsList PAGE</h1>
+    </div>);
+};
+
+const TopicDetial = (props) =>{
+    console.log(props)
+    return( <div>
+        <h1>TopicDetial PAGE: { props.match.params.topicId}</h1>
+    </div>);
+};
+
+const HatsPage = (props) =>{
+    console.log(props)
+    return( <div>
+        <h1>HatsPage PAGE</h1>
+    </div>);
+};
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      {/*<HomePage/>*/}
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/>
+      <Route exact path='/topics' component={TopicsList}/>
+          <Route path='/topics/:topicId' component={TopicDetial}/>
+      </Switch>
     </div>
   );
 }
--- crwn-clothing/src/pages/homepage/homepage.component.jsx
+++ crwn-clothing/src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './homepage.styles.scss';
-import MenuItem from '../../components/menu-item/memu-item.component'
+import Directory from "../../components/directory/directory.component";
 
 const HomePage = () =>(
     <div className='homepage'>
-        <div className='directory-menu'>
-                    <MenuItem title='HATS'/>
-            <MenuItem title='JACKT'/>
-        </div>
+        <Directory />
     </div>
 );
 
