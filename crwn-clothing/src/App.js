8d5d08e48abdd234c94a3f085fcd1907e5781e42
chengkrilo <krilo_c@hotmail>
2019-09-19 15:12:53 +0800

    collection-Item

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import {Switch , Route, Link} from 'react-router-dom';
 
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 // const HomePage = (props) =>{
 //     return(<div>
@@ -25,20 +26,13 @@ const TopicDetial = (props) =>{
     </div>);
 };
 
-const HatsPage = (props) =>{
-    console.log(props)
-    return( <div>
-        <h1>HatsPage PAGE</h1>
-    </div>);
-};
-
 function App() {
   return (
     <div>
       {/*<HomePage/>*/}
       <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={HatsPage}/>
+      <Route exact path='/shop' component={ShopPage}/>
       <Route exact path='/topics' component={TopicsList}/>
           <Route path='/topics/:topicId' component={TopicDetial}/>
       </Switch>
